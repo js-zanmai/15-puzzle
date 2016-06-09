c2ac1c006432d939c9d681aa15813b8dd996aa4d
Yoshitomo Minami <minami530@gmail.com>
2016-06-09 22:44:39 +0900

    見た目の悪さは置いておいて、サイズを変更できるようにした。vue.js使ってみた。

--- public/js/puzzle.js
+++ public/js/puzzle.js
@@ -2,29 +2,52 @@ var puzzle = (function() {
   'use strict';
   var
     size = 5,
-    createTable = function($container) {
-      var table = $('<table>').appendTo($container);
-      // size * size の分だけtd要素を作成する。
-      _(size).times(function(i) {
-        var tr = $('<tr>').appendTo(table);
-        _(size).times(function(j) {
-          var index = i * size + j;
-          $('<td>').addClass('tile').appendTo(tr)
-            .prop('index', index)
-            .prop('value', index)
-            .prop('textContent', index == 0 ? "" : index)
-            .on("click", function() {
-	            swap(this.index);
-            });	
-        });
-      });
+    initView = function() {
+      createTable();
+      randomizeTiles();
+
+      // ViewModelを初期化し、プルダウンメニューとバインディングする。
+      var setting = new Vue({
+        el: '#setting',
+        data: {
+          size: size
+      }});
       
-      // 適当な回数だけユーザー操作をシュミレートすることで、ランダムな配置にする。
-      _(5000).times(function() {
-        swap(_.random(size * size - 1));
+      // ViewModelの値が更新されたらサイズを更新し、再描画する。
+      setting.$watch('size', function(now, prev) {
+        console.log('changed:', prev, '->', now);
+        size = parseInt(now, 10);// サイズの更新
+        createTable();
+        randomizeTiles();
       });
     };
   
+  function createTable() {
+    $('#container table').remove();
+    var table = $('<table>').appendTo($('#container'));
+    // size * size の分だけtd要素を作成する。
+    _(size).times(function(i) {
+      var tr = $('<tr>').appendTo(table);
+      _(size).times(function(j) {
+        var index = i * size + j;
+        $('<td>').addClass('tile').appendTo(tr)
+          .prop('index', index)
+          .prop('value', index)
+          .prop('textContent', index == 0 ? "" : index)
+          .on("click", function() {
+            swap(this.index);
+          });
+      });
+    });
+  }
+
+  function randomizeTiles() {
+    // 適当な回数だけユーザー操作をシュミレートすることで、ランダムな配置にする。
+    _(5000).times(function() {
+      swap(_.random(size * size - 1));
+    });
+  }
+
   function swap(i) {
     var swapBody = function(i, j) {
       // i番目とj番目のtd要素の表示内容を入れ替える。
@@ -54,5 +77,5 @@ var puzzle = (function() {
     }
   }
     
-  return { init: createTable };
+  return { init: initView };
 }());
